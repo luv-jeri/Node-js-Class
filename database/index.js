341656704b5e69b59025fb9d07c4cbaa663dffa8
Ujjwal Patil <ujjwalpatl493@gmail.com>
2023-07-14 18:17:36 +0530

    Ujjwal added a message

--- database/index.js
+++ database/index.js
@@ -1,21 +1,21 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
 mongoose.connect(
-  'mongodb+srv://sanjay:luclasses@cluster0.ldnomgx.mongodb.net/space?retryWrites=true&w=majority',
+  "mongodb+srv://sanjay:luclasses@cluster0.ldnomgx.mongodb.net/space?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   }
 );
 
-mongoose.connection.on('connected', () => {
-  console.log('Connected to database');
+mongoose.connection.on("connected", () => {
+  console.log("Connected to database");
 });
 
-mongoose.connection.on('disconnected', function () {
-  console.log('Mongoose default connection disconnected');
+mongoose.connection.on("disconnected", function () {
+  console.log("Mongoose default connection disconnected");
 });
 
-mongoose.connection.on('error', (err) => {
-  console.log('Error connecting to database', err);
+mongoose.connection.on("error", (err) => {
+  console.log("Error connecting to database", err);
 });
